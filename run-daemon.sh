491f8bbc19aca13fd306bc22189ff6f3b4ad9555
Chris Tankersley <ctankersley@thebrickfactory.com>
2014-04-23 10:35:48 -0400

    Changed the logging to append instead of reset, because history is best (but not the channel. Stupid aliens)

--- run-daemon.sh
+++ run-daemon.sh
@@ -2,4 +2,4 @@
 PHP=/home/developer/local/bin/php55
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$PHP $DIR/unicron.php > $DIR/logs/unicron.log 2> $DIR/logs/unicron_error.log &
+$PHP $DIR/unicron.php >> $DIR/logs/unicron.log 2>> $DIR/logs/unicron_error.log &
